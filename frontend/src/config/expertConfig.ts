59b26457be3bfd4ed0e4e8e7fcea2974e92b6fed
Andrii Sazonov <literavision@gmail.com>
2026-03-03 13:05:10 +0100

    feat: add new expert ostrikov and move old export to archive

--- frontend/src/config/expertConfig.ts
+++ frontend/src/config/expertConfig.ts
@@ -19,7 +19,7 @@ export interface ExpertGroup {
  * Expert Groups Definition (Used in Sidebar and Mobile Selection)
  */
 export const EXPERT_GROUPS: ExpertGroup[] = [
-  { label: 'Tech', expertIds: ['ai_architect', 'neuraldeep', 'ilia_izmailov', 'polyakov', 'etechlead', 'glebkudr'] },
+  { label: 'Tech', expertIds: ['ai_architect', 'neuraldeep', 'ilia_izmailov', 'polyakov', 'etechlead', 'glebkudr', 'ostrikov'] },
   { label: 'Tech & Business', expertIds: ['ai_grabli', 'refat', 'akimov', 'llm_under_hood', 'elkornacio', 'doronin', 'air_ai'] },
   { label: 'Knowledge Hub', expertIds: ['video_hub'] },
 ];
@@ -43,10 +43,11 @@ export const EXPERT_UI_CONFIG: ExpertUIConfig = {
     'etechlead': 'Etechlead',
     'glebkudr': 'Glebkudr',
     'video_hub': 'Video_Hub',
-    'air_ai': 'Air'
+    'air_ai': 'Air',
+    'ostrikov': 'Ostrikov'
   },
   // Order used for sorting results
-  order: ['refat', 'ai_architect', 'neuraldeep', 'ai_grabli', 'akimov', 'llm_under_hood', 'elkornacio', 'ilia_izmailov', 'polyakov', 'doronin', 'etechlead', 'glebkudr', 'video_hub', 'air_ai']
+  order: ['refat', 'ai_architect', 'neuraldeep', 'ai_grabli', 'akimov', 'llm_under_hood', 'elkornacio', 'ilia_izmailov', 'polyakov', 'doronin', 'etechlead', 'glebkudr', 'video_hub', 'air_ai', 'ostrikov']
 };
 
 /**
